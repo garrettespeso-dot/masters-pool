b62ff98ccabb0c33e8ca4d47afffe98ac8f17a4a
garrettespeso-dot <garrett.espeso@gmail.com>
2026-04-10 10:50:21 -0400

    Add participants with their picks for buckets

--- src/participants.js
+++ src/participants.js
@@ -78,5 +78,305 @@ export const DEFAULT_PARTICIPANTS = [
       "Bucket 4": ["Jordan Spieth","Tom Kim"],
       "Bucket 5": ["Ben Griffin","Ryan Gerard"]
     }
+  },
+  {
+    name: "Mark Kuether",
+    picks: {
+      "Bucket 1": ["Scottie Scheffler", "Bryson DeChambeau"],
+      "Bucket 2": ["Robert MacIntyre", "Patrick Reed"],
+      "Bucket 3": ["Akshay Bhatia", "Nicolai Hojgaard"],
+      "Bucket 4": ["Chris Gotterup", "Daniel Berger"],
+      "Bucket 5": ["Michael Brennan", "Cameron Smith"]
+    }
+  },
+  {
+    name: "Greg Thomas",
+    picks: {
+      "Bucket 1": ["Scottie Scheffler", "Bryson DeChambeau"],
+      "Bucket 2": ["Justin Rose", "Hideki Matsuyama"],
+      "Bucket 3": ["Patrick Cantlay", "Si Woo Kim"],
+      "Bucket 4": ["Jordan Spieth", "Brooks Koepka"],
+      "Bucket 5": ["Matt McCarty", "Dustin Johnson"]
+    }
+  },
+  {
+    name: "TJ #1",
+    picks: {
+      "Bucket 1": ["Scottie Scheffler", "Jon Rahm"],
+      "Bucket 2": ["Justin Rose", "Viktor Hovland"],
+      "Bucket 3": ["Patrick Cantlay", "Sungjae Im"],
+      "Bucket 4": ["Brooks Koepka", "Max Homa"],
+      "Bucket 5": ["Jake Knapp", "Michael Kim"]
+    }
+  },
+  {
+    name: "TJ #2",
+    picks: {
+      "Bucket 1": ["Matt Fitzpatrick", "Tommy Fleetwood"],
+      "Bucket 2": ["Robert MacIntyre", "Tyrrell Hatton"],
+      "Bucket 3": ["Sepp Straka", "Si Woo Kim"],
+      "Bucket 4": ["Gary Woodland", "Jacob Bridgeman"],
+      "Bucket 5": ["Brian Harman", "Ben Griffin"]
+    }
+  },
+  {
+    name: "Bontempo",
+    picks: {
+      "Bucket 1": ["Jon Rahm", "Xander Schauffele"],
+      "Bucket 2": ["Justin Rose", "Patrick Reed"],
+      "Bucket 3": ["Akshay Bhatia", "Adam Scott"],
+      "Bucket 4": ["Chris Gotterup", "Jordan Spieth"],
+      "Bucket 5": ["Marco Penge", "Cameron Smith"]
+    }
+  },
+  {
+    name: "Chelsea Tacey",
+    picks: {
+      "Bucket 1": ["Scottie Scheffler", "Bryson DeChambeau"],
+      "Bucket 2": ["Hideki Matsuyama", "Justin Thomas"],
+      "Bucket 3": ["Russell Henley", "Akshay Bhatia"],
+      "Bucket 4": ["Jordan Spieth", "Max Homa"],
+      "Bucket 5": ["Jake Knapp", "Andrew Novak"]
+    }
+  },
+  {
+    name: "C Bade",
+    picks: {
+      "Bucket 1": ["Ludvig Aberg", "Xander Schauffele"],
+      "Bucket 2": ["Patrick Reed", "Tyrrell Hatton"],
+      "Bucket 3": ["Corey Conners", "Akshay Bhatia"],
+      "Bucket 4": ["Chris Gotterup", "Daniel Berger"],
+      "Bucket 5": ["Ben Griffin", "Cameron Smith"]
+    }
+  },
+  {
+    name: "Espeso",
+    picks: {
+      "Bucket 1": ["Scottie Scheffler", "Cameron Young"],
+      "Bucket 2": ["Justin Rose", "JJ Spaun"],
+      "Bucket 3": ["Russell Henley", "Sepp Straka"],
+      "Bucket 4": ["Chris Gotterup", "Jacob Bridgeman"],
+      "Bucket 5": ["Ryan Gerard", "Ben Griffin"]
+    }
+  },
+  {
+    name: "D Bade",
+    picks: {
+      "Bucket 1": ["Scottie Scheffler", "Ludvig Aberg"],
+      "Bucket 2": ["Justin Rose", "Hideki Matsuyama"],
+      "Bucket 3": ["Sepp Straka", "Akshay Bhatia"],
+      "Bucket 4": ["Gary Woodland", "Brooks Koepka"],
+      "Bucket 5": ["Jake Knapp", "Ben Griffin"]
+    }
+  },
+  {
+    name: "Perrotti",
+    picks: {
+      "Bucket 1": ["Scottie Scheffler", "Rory McIlroy"],
+      "Bucket 2": ["Viktor Hovland", "Shane Lowry"],
+      "Bucket 3": ["Corey Conners", "Si Woo Kim"],
+      "Bucket 4": ["Brooks Koepka", "Max Homa"],
+      "Bucket 5": ["Cameron Smith", "Dustin Johnson"]
+    }
+  },
+  {
+    name: "AA",
+    picks: {
+      "Bucket 1": ["Scottie Scheffler", "Ludvig Aberg"],
+      "Bucket 2": ["Robert MacIntyre", "Hideki Matsuyama"],
+      "Bucket 3": ["Russell Henley", "Akshay Bhatia"],
+      "Bucket 4": ["Chris Gotterup", "Brooks Koepka"],
+      "Bucket 5": ["Jake Knapp", "Ben Griffin"]
+    }
+  },
+  {
+    name: "Shannon",
+    picks: {
+      "Bucket 1": ["Scottie Scheffler", "Rory McIlroy"],
+      "Bucket 2": ["Justin Rose", "Patrick Reed"],
+      "Bucket 3": ["Si Woo Kim", "Adam Scott"],
+      "Bucket 4": ["Harris English", "Brooks Koepka"],
+      "Bucket 5": ["Jake Knapp", "Andrew Novak"]
+    }
+  },
+  {
+    name: "Hanecak",
+    picks: {
+      "Bucket 1": ["Bryson DeChambeau", "Rory McIlroy"],
+      "Bucket 2": ["Justin Rose", "Hideki Matsuyama"],
+      "Bucket 3": ["Patrick Cantlay", "Adam Scott"],
+      "Bucket 4": ["Jordan Spieth", "Max Homa"],
+      "Bucket 5": ["Jake Knapp", "Dustin Johnson"]
+    }
+  },
+  {
+    name: "Pineau",
+    picks: {
+      "Bucket 1": ["Xander Schauffele", "Cameron Young"],
+      "Bucket 2": ["Min Woo Lee", "Hideki Matsuyama"],
+      "Bucket 3": ["Akshay Bhatia", "Maverick McNealy"],
+      "Bucket 4": ["Chris Gotterup", "Max Homa"],
+      "Bucket 5": ["Jake Knapp", "Ben Griffin"]
+    }
+  },
+  {
+    name: "Bryan Dinnis",
+    picks: {
+      "Bucket 1": ["Scottie Scheffler", "Bryson DeChambeau"],
+      "Bucket 2": ["Patrick Reed", "Tyrrell Hatton"],
+      "Bucket 3": ["Corey Conners", "Russell Henley"],
+      "Bucket 4": ["Chris Gotterup", "Aaron Rai"],
+      "Bucket 5": ["Jake Knapp", "Marco Penge"]
+    }
+  },
+  {
+    name: "Bryan Sevigny",
+    picks: {
+      "Bucket 1": ["Scottie Scheffler", "Bryson DeChambeau"],
+      "Bucket 2": ["Justin Rose", "Hideki Matsuyama"],
+      "Bucket 3": ["Russell Henley", "Si Woo Kim"],
+      "Bucket 4": ["Chris Gotterup", "Brooks Koepka"],
+      "Bucket 5": ["Jake Knapp", "Marco Penge"]
+    }
+  },
+  {
+    name: "Nick Sibley",
+    picks: {
+      "Bucket 1": ["Scottie Scheffler", "Bryson DeChambeau"],
+      "Bucket 2": ["Viktor Hovland", "Hideki Matsuyama"],
+      "Bucket 3": ["Jason Day", "Si Woo Kim"],
+      "Bucket 4": ["Jordan Spieth", "Brooks Koepka"],
+      "Bucket 5": ["Cameron Smith", "Dustin Johnson"]
+    }
+  },
+  {
+    name: "Luke M",
+    picks: {
+      "Bucket 1": ["Scottie Scheffler", "Cameron Young"],
+      "Bucket 2": ["Min Woo Lee", "JJ Spaun"],
+      "Bucket 3": ["Russell Henley", "Akshay Bhatia"],
+      "Bucket 4": ["Chris Gotterup", "Brooks Koepka"],
+      "Bucket 5": ["Jake Knapp", "Cameron Smith"]
+    }
+  },
+  {
+    name: "Geoff Brimecombe",
+    picks: {
+      "Bucket 1": ["Scottie Scheffler", "Cameron Young"],
+      "Bucket 2": ["Justin Rose", "Min Woo Lee"],
+      "Bucket 3": ["Akshay Bhatia", "Adam Scott"],
+      "Bucket 4": ["Gary Woodland", "Brooks Koepka"],
+      "Bucket 5": ["Brian Harman", "Cameron Smith"]
+    }
+  },
+  {
+    name: "Kosta T",
+    picks: {
+      "Bucket 1": ["Ludvig Aberg", "Xander Schauffele"],
+      "Bucket 2": ["Patrick Reed", "Viktor Hovland"],
+      "Bucket 3": ["Russell Henley", "Si Woo Kim"],
+      "Bucket 4": ["Chris Gotterup", "Brooks Koepka"],
+      "Bucket 5": ["Jake Knapp", "Marco Penge"]
+    }
+  },
+  {
+    name: "Braden Quinn",
+    picks: {
+      "Bucket 1": ["Jon Rahm", "Bryson DeChambeau"],
+      "Bucket 2": ["Min Woo Lee", "Hideki Matsuyama"],
+      "Bucket 3": ["Corey Conners", "Akshay Bhatia"],
+      "Bucket 4": ["Daniel Berger", "Jordan Spieth"],
+      "Bucket 5": ["Ben Griffin", "Cameron Smith"]
+    }
+  },
+  {
+    name: "Gunnar",
+    picks: {
+      "Bucket 1": ["Scottie Scheffler", "Jon Rahm"],
+      "Bucket 2": ["Patrick Reed", "Hideki Matsuyama"],
+      "Bucket 3": ["Patrick Cantlay", "Sungjae Im"],
+      "Bucket 4": ["Brooks Koepka", "Max Homa"],
+      "Bucket 5": ["Cameron Smith", "Dustin Johnson"]
+    }
+  },
+  {
+    name: "Matt Baroni",
+    picks: {
+      "Bucket 1": ["Bryson DeChambeau", "Ludvig Aberg"],
+      "Bucket 2": ["Justin Rose", "Robert MacIntyre"],
+      "Bucket 3": ["Patrick Cantlay", "Akshay Bhatia"],
+      "Bucket 4": ["Chris Gotterup", "Brooks Koepka"],
+      "Bucket 5": ["Andrew Novak", "Nico Echavarria"]
+    }
+  },
+  {
+    name: "Prachi",
+    picks: {
+      "Bucket 1": ["Scottie Scheffler", "Bryson DeChambeau"],
+      "Bucket 2": ["Robert MacIntyre", "Min Woo Lee"],
+      "Bucket 3": ["Akshay Bhatia", "Si Woo Kim"],
+      "Bucket 4": ["Jacob Bridgeman", "Max Homa"],
+      "Bucket 5": ["Matt McCarty", "Nico Echavarria"]
+    }
+  },
+  {
+    name: "Matt Beagle",
+    picks: {
+      "Bucket 1": ["Scottie Scheffler", "Xander Schauffele"],
+      "Bucket 2": ["Viktor Hovland", "Hideki Matsuyama"],
+      "Bucket 3": ["Patrick Cantlay", "Russell Henley"],
+      "Bucket 4": ["Brooks Koepka", "Max Homa"],
+      "Bucket 5": ["Brian Harman", "Cameron Smith"]
+    }
+  },
+  {
+    name: "PJ",
+    picks: {
+      "Bucket 1": ["Scottie Scheffler", "Ludvig Aberg"],
+      "Bucket 2": ["Min Woo Lee", "Justin Thomas"],
+      "Bucket 3": ["Sepp Straka", "Akshay Bhatia"],
+      "Bucket 4": ["Chris Gotterup", "Brooks Koepka"],
+      "Bucket 5": ["Michael Kim", "Cameron Smith"]
+    }
+  },
+  {
+    name: "Hanbury",
+    picks: {
+      "Bucket 1": ["Scottie Scheffler", "Matt Fitzpatrick"],
+      "Bucket 2": ["Justin Rose", "Tyrrell Hatton"],
+      "Bucket 3": ["Sepp Straka", "Nicolai Hojgaard"],
+      "Bucket 4": ["Harris English", "Chris Gotterup"],
+      "Bucket 5": ["Jake Knapp", "Brian Harman"]
+    }
+  },
+  {
+    name: "Pat Keane",
+    picks: {
+      "Bucket 1": ["Ludvig Aberg", "Cameron Young"],
+      "Bucket 2": ["Hideki Matsuyama", "Tyrrell Hatton"],
+      "Bucket 3": ["Akshay Bhatia", "Si Woo Kim"],
+      "Bucket 4": ["Gary Woodland", "Chris Gotterup"],
+      "Bucket 5": ["Jake Knapp", "Cameron Smith"]
+    }
+  },
+  {
+    name: "Ragonese",
+    picks: {
+      "Bucket 1": ["Scottie Scheffler", "Rory McIlroy"],
+      "Bucket 2": ["Min Woo Lee", "Hideki Matsuyama"],
+      "Bucket 3": ["Akshay Bhatia", "Keegan Bradley"],
+      "Bucket 4": ["Jordan Spieth", "Brooks Koepka"],
+      "Bucket 5": ["Marco Penge", "Dustin Johnson"]
+    }
+  },
+  {
+    name: "Jim Lemke",
+    picks: {
+      "Bucket 1": ["Scottie Scheffler", "Rory McIlroy"],
+      "Bucket 2": ["Hideki Matsuyama", "Justin Thomas"],
+      "Bucket 3": ["Akshay Bhatia", "Sungjae Im"],
+      "Bucket 4": ["Chris Gotterup", "Max Homa"],
+      "Bucket 5": ["Jake Knapp", "Andrew Novak"]
+    }
   }
 ];
